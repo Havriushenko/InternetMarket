aad876fca3d206e2b25b4f4e862c052396fd9fb6
Mykyta <nik.gavrushenko@gmail.com>
2020-05-25 14:05:17 +0300

    Add tests for order service

--- src/test/java/pro_area/test_task/havriushenko/internet_market/service/OrderServiceTest.java
+++ src/test/java/pro_area/test_task/havriushenko/internet_market/service/OrderServiceTest.java
@@ -20,6 +20,9 @@ import java.util.Collections;
 import java.util.Optional;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static pro_area.test_task.havriushenko.internet_market.util.Constans.MESSAGE_YOUR_ORDER_IS_EMPTY;
 import static pro_area.test_task.havriushenko.internet_market.utils.ConstansForTest.*;
@@ -27,6 +30,7 @@ import static pro_area.test_task.havriushenko.internet_market.utils.ConstansForT
 @RunWith(JUnit4.class)
 public class OrderServiceTest {
 
+    public static final int TEST_QUANTITY = 10;
     @Mock
     private UserService userService;
     @Mock
@@ -40,6 +44,7 @@ public class OrderServiceTest {
     private UserDto userDto;
     private OrderModel orderModel;
     private OrderDto orderDto;
+    private OrderDto closeOrderDto;
 
     private OrderService tested;
 
@@ -47,9 +52,9 @@ public class OrderServiceTest {
     public void setUp() {
         MockitoAnnotations.initMocks(this);
         createUserModel();
-        createUserDto();
+        createUserDtos();
         createOrderModel();
-        createOrderDto();
+        createOrderDtos();
 
         tested = new OrderService(userService, orderRepository, orderConverter, orderInfoRepository);
     }
@@ -64,7 +69,7 @@ public class OrderServiceTest {
         userModel.setRole(Collections.singleton(Role.USER));
     }
 
-    private void createUserDto() {
+    private void createUserDtos() {
         userDto = new UserDto();
         userDto.setId(FIRST_TEST_ID);
         userDto.setName(FIRST_TEST_USER_NAME);
@@ -82,12 +87,18 @@ public class OrderServiceTest {
         orderModel.setOrderInfoModels(Collections.EMPTY_SET);
     }
 
-    private void createOrderDto() {
+    private void createOrderDtos() {
         orderDto = new OrderDto();
         orderDto.setId(FIRST_TEST_ID);
         orderDto.setUser(userDto);
         orderDto.setStatus(true);
-        orderModel.setOrderInfoModels(Collections.EMPTY_SET);
+        orderDto.setProducts(Collections.EMPTY_MAP);
+
+        closeOrderDto = new OrderDto();
+        closeOrderDto.setId(FIRST_TEST_ID);
+        closeOrderDto.setUser(userDto);
+        closeOrderDto.setStatus(false);
+        closeOrderDto.setProducts(Collections.EMPTY_MAP);
     }
 
     @Test
@@ -113,5 +124,48 @@ public class OrderServiceTest {
 
             assertEquals(MESSAGE_YOUR_ORDER_IS_EMPTY, result);
         }
+    }
+
+    @Test
+    public void putProductToOrderOneAndShouldCallUpsertMethodFromRepo() {
+        when(userService.getUserModelByEmail(FIRST_TEST_USER_EMAIL)).thenReturn(userModel);
+        when(orderRepository.findOrderByUserIdAndStatus(userDto.getId(), true)).thenReturn(Optional.ofNullable(orderModel));
+
+        tested.putProductToOrder(FIRST_TEST_USER_EMAIL, FIRST_TEST_ID, TEST_QUANTITY);
+
+        verify(orderInfoRepository).upsertOrderInfoByOrderIdAndProductId(orderDto.getId(), FIRST_TEST_ID, TEST_QUANTITY);
+    }
+
+    @Test
+    public void putProductInNewOrder() {
+        when(userService.getUserModelByEmail(FIRST_TEST_USER_EMAIL)).thenReturn(userModel);
+        when(orderRepository.save(any())).thenReturn(new OrderModel(true, userModel));
+
+        tested.putProductToOrder(FIRST_TEST_USER_EMAIL, FIRST_TEST_ID, TEST_QUANTITY);
+
+        verify(orderRepository).save(any());
+        verify(orderInfoRepository).upsertOrderInfoByOrderIdAndProductId(0, FIRST_TEST_ID, TEST_QUANTITY);
+    }
+
+    @Test
+    public void deleteProductWithIdOneFromOrder() {
+        when(userService.getUserModelByEmail(FIRST_TEST_USER_EMAIL)).thenReturn(userModel);
+        when(orderRepository.findOrderByUserIdAndStatus(userModel.getId(),true)).thenReturn(Optional.ofNullable(orderModel));
+
+        tested.deleteProductFromOrder(FIRST_TEST_USER_EMAIL, SECOND_TEST_ID);
+
+        verify(orderInfoRepository).deleteByOrderIdAndProductId(orderModel.getId(), SECOND_TEST_ID);
+    }
+
+    @Test
+    public void getOrderForReceiptAndCloseThisOrder(){
+        when(userService.findByEmail(FIRST_TEST_USER_EMAIL)).thenReturn(userDto);
+        when(orderRepository.findOrderByUserIdAndStatus(userDto.getId(),true)).thenReturn(Optional.ofNullable(orderModel));
+        when(orderConverter.convertToDto(any())).thenReturn(closeOrderDto);
+
+        OrderDto result = tested.getOrderForReceipt(FIRST_TEST_USER_EMAIL);
+
+        assertFalse(result.getStatus());
+        verify(orderRepository).save(any());
     }
 }
